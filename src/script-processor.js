9c969c6245544b7958814a23a085eceb9dd7a48c
mosm <airman515@gmail.com>
2020-10-21 07:58:15 +0900

    Replace non-finite values with zero
    Non-finite values make spectrum analyzer stop working

--- src/script-processor.js
+++ src/script-processor.js
@@ -1,15 +1,25 @@
 class ScriptProcessor extends AudioWorkletProcessor {
-    constructor() {
-        super();
-    }
-
     process(_inputs, outputs) {
         if (typeof loop === 'undefined') {
             return false;
         }
 
-        const out = outputs[0];
-        loop(out[0].length, out[0], out[1]);
+        const outL = outputs[0][0];
+        const outR = outputs[0][1];
+
+        loop(outL.length, outL, outR);
+
+        for (let i = 0; i < outL.length; ++i) {
+            if (!Number.isFinite(outL[i])) {
+                outL[i] = 0;
+            }
+        }
+        for (let i = 0; i < outR.length; ++i) {
+            if (!Number.isFinite(outR[i])) {
+                outR[i] = 0;
+            }
+        }
+
         return true;
     }
 }
